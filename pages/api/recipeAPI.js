4aa7ea8ba1cb48fadff43c84557027ed73d4f4c4
Justtin Hoang <daminhjusttinhoang11@gmail.com>
2024-09-26 21:04:29 -0400

    add app id

--- pages/api/recipeAPI.js
+++ pages/api/recipeAPI.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const recipeKey = process.env.RECIPE;
-  const recipeID = process.env.RECIPE;
+  const recipeID = process.env.RECIPE_ID;
   const {query} = req.query;
   if (!query) {
     return res.status(400).json({ error: "No recipe query provided" });
